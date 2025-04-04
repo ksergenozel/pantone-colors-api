eb4df89ea7bf4de0ef44fe421f126dbe85fb9cae
ksergenozel <ksergenozel@gmail.com>
2025-04-05 01:09:40 +0300

    fix: getColors sql query

--- src/modules/color/service.ts
+++ src/modules/color/service.ts
@@ -3,17 +3,7 @@ import { Color } from "./model.js";
 export const getColors = (limit: number = 100, offset: number = 0): Color[] => {
   const maxLimit = Math.min(limit, 100);
   const query = `
-    SELECT * FROM colors
-    ORDER BY 
-      CASE collection
-        WHEN 'Fashion & Interior Designers' THEN 1
-        WHEN 'Graphic Designers' THEN 2
-        WHEN 'Industrial Designers' THEN 3
-        ELSE 99
-      END,
-      (name = '') ASC,
-      name ASC
-    LIMIT ? OFFSET ?
+    SELECT * FROM colors ORDER BY collection ASC LIMIT ? OFFSET ?
   `;
   return db.prepare(query).all(maxLimit, offset) as Color[];
 };
